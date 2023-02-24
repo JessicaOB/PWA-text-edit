2817b7c768ccd0189a650f5d527382d23e6ab370
Jessica Oboh <jessicarichardson@Jessicas-MBP.lan>
2023-02-24 15:27:12 -0700

    set up webpack and database

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,30 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  //create connection to db
+  const jateDB = await openDB('jate', 1);
+  const tx = jateDB.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  //use put method to update content
+  const request = store.put({ jate: content });
+  const result = await request;
+
+  console.log("data saved", result);
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  //create connection to db
+  const jateDB = await openDB("jate", 1);
+  const tx = jateDB .transaction("jate", "readonly");
+  const store = tx.objectStore("jate");
+  //get the data
+  const request = store.getAll();
+  //wait for result
+  const result = await request;
+  console.log('result.value',result);
+  return result;
+};
 
 initdb();
